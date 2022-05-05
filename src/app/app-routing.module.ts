d7cc6e2fdf7c20ed4217f7d93d14723b58955f6a
Stefano Belli <56109150+bstefano79@users.noreply.github.com>
2022-05-05 10:33:22 +0200

    componente iscrizione e routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IscrizioneComponent } from './iscrizione/iscrizione.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'iscriviti', component: IscrizioneComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
